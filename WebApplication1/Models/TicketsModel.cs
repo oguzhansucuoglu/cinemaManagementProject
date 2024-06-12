fcd43b100913443197180ccb5fafb6c46669b127
Oğuzhan Sucuoğlu <oguzhansucu74@gmail.com>
2024-06-12 12:13:05 +0300

    Added ticket model

---
+++ WebApplication1/Models/TicketsModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Tickets")]
+    public class TicketsModel
+    {
+        public Int32 ticket_id { get; set; }
+        public Int32 movie_id { get; set; }
+        public Int32 theater_id { get; set; }
+        public Int32 seat_id { get; set; }
+        public Int32 hall_id { get; set; }
+        public DateTime showtime { get; set; }
+    }
+}
